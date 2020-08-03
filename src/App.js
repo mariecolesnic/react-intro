4161c3bddd75aeb522607fc55dde0b9273510bda
Maria Colesnic <melterina96@gmail.com>
2020-08-03 14:12:48 +0300

    Miscellaneous changes

--- src/App.js
+++ src/App.js
@@ -1,28 +1,26 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Greetings from "./Greetings";
 
-class App extends Component {
-  render() {
-    return (
+const App = () => (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <p>
+            Marie has just started studying React. 
             Edit <code>src/App.js</code> and save to reload.
           </p>
           <a
             className="App-link"
-            href="https://reactjs.org"
+            href="https://vk.com/mariecolesnic"
             target="_blank"
             rel="noopener noreferrer"
           >
-            Learn React
+            Visit Marie's VK
           </a>
         </header>
       </div>
-    );
-  }
-}
+);
 
 export default App;
