fe2bd8791c236b132e4b7525f4b0ffd3838c5ba7
Maria Colesnic <melterina96@gmail.com>
2020-08-03 18:41:10 +0300

    Added style for form

---
+++ src/style.js
@@ -0,0 +1,23 @@
+const style = {
+    form: {
+        margin: 50,
+        padding: 10,
+        width: 300,
+        border: "10px solid black",
+        backgroundColor: "blck",
+        color: "white"
+    },
+    inputGroup: {
+        marginBottom: 10
+    },
+    input: {
+        backgroundColor: "",
+        marginLeft: 10
+    },
+    error: {
+        color: "red",
+        margin: 5
+    }
+};
+
+export default style;
